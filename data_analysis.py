2d4f8627e1446ec3aaee4cbaf6ca0b5728ab3fc3
Pep Creus Costa <pep.creuscosta@gmail.com>
2019-07-15 16:38:06 +0200

    Added more code lines to plot the predicted results.

--- data_analysis.py
+++ data_analysis.py
@@ -11,11 +11,19 @@ train_y = train.iloc[:,-1].apply(lambda x: 1 if x == 'M' else 0)
 train_x = train.iloc[:,:-1]
 
 print (train_x.head())
-
 rm_linear = LinearRegression().fit(train_x, train_y)
 
 test_x = test.iloc[:,:-1]
 test_yhat = rm_linear.predict(test_x)
+test_y = test.iloc[:,-1].apply(lambda x: 1 if x == 'M' else 0)
+gamma = 0.4
+test_decisions = np.where(test_yhat < gamma, 0, 1)
+
+print (test_yhat)
+print (test_y)
+print(np.abs((test_yhat - test_y) ** 2))
+print (test_decisions)
+print (rm_linear.score(test_x, test_y))
 
 train.head()
 test.head()
