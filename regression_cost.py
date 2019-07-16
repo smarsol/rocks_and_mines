ad757ead6070d41810543188215c2e524137c07b
Pep Creus Costa <pep.creuscosta@gmail.com>
2019-07-16 14:49:47 +0200

    Added code lines to plot the cost of detection of rocks and mines.

---
+++ regression_cost.py
@@ -0,0 +1,96 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.model_selection import train_test_split
+
+def confusion_matrix(predicted, real, gamma):
+  test_decisions = np.where(predicted < gamma, 0, 1)  
+  tp = np.logical_and(test_decisions == 1, real == 1).sum()
+  fp = np.logical_and(test_decisions == 1, real == 0).sum()
+  tn = np.logical_and(test_decisions == 0, real == 0).sum()
+  fn = np.logical_and(test_decisions == 0, real == 1).sum()
+  return tp, fp, tn, fn
+
+def false_positives(model, test):
+  gamma = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+  llista = []
+  for x in gamma:
+    test_x = test.iloc[:,:-1]
+    test_yhat = model.predict(test_x)
+    test_y = test.iloc[:,-1].apply(lambda x: 1 if x == 'M' else 0)
+    test_decisions = np.where(test_yhat < x, 0, 1)
+    llista.append(np.logical_and(test_decisions == 1, test_y == 0).sum())
+  plt.scatter(gamma, llista)
+  plt.show()
+  return llista, gamma
+
+data = pd.read_csv('sonar.all-data', header = None)
+train, test = train_test_split(data, test_size = 0.2)
+
+train_y = train.iloc[:,-1].apply(lambda x: 1 if x == 'M' else 0)
+train_x = train.iloc[:,:-1]
+
+print (train_x.head())
+model = LinearRegression().fit(train_x, train_y)
+test_x = test.iloc[:,:-1]
+test_yhat = model.predict(test_x)
+test_y = test.iloc[:,-1].apply(lambda x: 1 if x == 'M' else 0)
+gamma = 0.25
+test_decisions = np.where(test_yhat < gamma, 0, 1)
+
+print (test_yhat)
+print (test_y)
+print(np.abs((test_yhat - test_y) ** 2))
+print (test_decisions)
+print (model.score(test_x, test_y))
+
+train.head()
+test.head()
+
+# print(false_positives(model, test))
+print(confusion_matrix(test_yhat, test_y, gamma))
+
+train_yhat = model.predict(train_x)
+
+tpr_train = []
+fpr_train = []
+fpr_test = []
+tpr_test = []
+
+for gamma_test in np.arange(-5, 5, 0.1):
+  tp_train, fp_train, tn_train, fn_train = confusion_matrix(train_yhat, train_y, gamma_test)
+  tpr_train.append(tp_train/(tp_train + fn_train))
+  fpr_train.append(fp_train/(tn_train + fp_train))
+  tp_test, fp_test, tn_test, fn_test = confusion_matrix(test_yhat, test_y, gamma_test)
+  tpr_test.append(tp_test/(tp_test + fn_test))
+  fpr_test.append(fp_test/(tn_test + fp_test))
+
+print(tpr_test)
+print(fpr_test) 
+
+#plt.scatter(fpr_train, tpr_train, c = 'b')
+#plt.scatter(fpr_test, tpr_test, c = 'r')
+#plt.show()
+cost_total = []
+gammas = np.arange(-0.5, 1.5, 0.1)
+total = len(train_y)
+for gamma_test in gammas:
+  tp, fp, tn, fn = confusion_matrix(train_yhat, train_y, gamma_test)
+  cost_total.append((tn * 200 +fn * 1200 + tp * 1000 + fp * 300)/total)
+
+fig = plt.figure()
+plt.plot(gammas, cost_total, 'o--')
+plt.xlabel(u"Gamma [\u03B3]", fontsize = 18)
+plt.ylabel("Cost total (euros)", fontsize = 18)
+fig.tight_layout()
+fig.savefig("figure_cost.png")
+
+plt.show()
+
+# f_4 = data.iloc[:,10]
+# f_24 = data.iloc[:,33]
+# labels = data.iloc[:,-1]
+
+# plt.scatter(f_4, f_24, c = labels)
+# plt.show()
